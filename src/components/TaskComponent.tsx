e463a4d3863841420bca8cb81c3d3192ce30cb3a
Fran <kezicfran@gmail.com>
2023-10-26 23:34:23 +0200

    Synced firebase backend with client-side actions

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,6 +1,13 @@
 import { SetStateAction, useEffect, useState } from 'react';
 import { Task } from './types';
-import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  limit,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 import useLocalStorage from '@/customHooks/useLocalStorage';
 
@@ -28,8 +35,7 @@ export default function TaskComponent({
     return () => clearInterval(intervalId);
   }, [task.isRunning]);
 
-  {
-    /*async function handleDatabaseStartPause() {
+  async function handleDatabaseStartPause() {
     // always inverse isRunning
     // if starting a stopwatch set lastResumed to Date.now()
     // if we stop a timer, update accumulatedTime
@@ -38,38 +44,44 @@ export default function TaskComponent({
       const usersRef = collection(db, 'users');
       const q = query(usersRef, where('username', '==', user), limit(1));
       const qSnap = await getDocs(q);
-      const tasksArray = qSnap.docs[0].data().tasks;
+      if (!qSnap.empty) {
+        const userDoc = qSnap.docs[0];
+        if (userDoc.exists()) {
+          const tasksArray = userDoc.data().tasks;
 
-      const updatedTasks = tasksArray.map((t: Task) => {
-        if (t.startTime === task.startTime) {
-          const currentTime = Date.now();
-          if (task.isRunning) {
-            // we are stopping it now
-            return {
-              ...t,
-              isRunning: false,
-              accumulatedTime:
-                t.accumulatedTime + (currentTime - t.lastResumed),
-            };
-          } else {
-            // we are starting the stopwatch
-            return {
-              ...t,
-              isRunning: true,
-              lastResumed: currentTime,
-            };
-          }
-        } else {
-          return t;
+          const updatedTasks = tasksArray.map((t: Task) => {
+            if (t.startTime === task.startTime) {
+              const currentTime = Date.now();
+              if (task.isRunning) {
+                // stopping the timer now
+                return {
+                  ...t,
+                  isRunning: false,
+                  accumulatedTime:
+                    t.accumulatedTime + (currentTime - t.lastResumed),
+                };
+              } else {
+                // we are starting the stopwatch
+                return {
+                  ...t,
+                  isRunning: true,
+                  lastResumed: currentTime,
+                };
+              }
+            } else {
+              return t;
+            }
+          });
+          await updateDoc(userDoc.ref, { tasks: updatedTasks });
         }
-      });
+      }
     } catch (error) {
       console.log(`Failed to fetch user\'s tasks. Error: ${error}`);
     }
-  }*/
   }
 
   function handleStartPause() {
+    handleDatabaseStartPause();
     setTasks((prevTasks) => {
       return prevTasks.map((t) => {
         if (t.startTime === task.startTime) {
@@ -107,7 +119,7 @@ export default function TaskComponent({
       <div className="w-3/5 flex items-center pl-4 border border-gray-100 h-14">
         Task 123 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       </div>
-      <div className="w-1/5 flex items-center justify-evenly pl-4 border border-gray-100 h-14 text-xl">
+      <div className="w-1/5 flex items-center justify-evenly border border-gray-100 h-14 text-xl">
         <button onClick={handleStartPause}>
           <span className="pi pi-pause" />
         </button>
